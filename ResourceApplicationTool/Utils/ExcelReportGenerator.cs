caccbeb90e7db4bd5155e121c552027f9369d6a1
serafim_tanasescu <radu_mihai.bucur@yahoo.com>
2017-04-13 10:46:59 +0300

    Data retrieval for Excel Report

--- ResourceApplicationTool/Utils/ExcelReportGenerator.cs
+++ ResourceApplicationTool/Utils/ExcelReportGenerator.cs
@@ -21,6 +21,41 @@ namespace ResourceApplicationTool.Utils
     {
         public static byte[] GenerateExcelReport(int ProjectID, int month, int year, RATV3Entities db)
         {
+
+            Project project = db.Projects.Where(x => x.ProjectID == ProjectID).FirstOrDefault();
+            if(project!= null)
+            {
+                #region GetData
+                //getting the department
+                Department department = db.Departments.Where(x => x.DepartmentID == project.DepartmentID).FirstOrDefault();
+                
+                //setting the date interval for our Sprints
+                DateTime firstDayOfMonth = new DateTime(year, month, 1);
+                DateTime lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddTicks(-1);
+
+                while (firstDayOfMonth.DayOfWeek != DayOfWeek.Monday)
+                {
+                    firstDayOfMonth = firstDayOfMonth.AddDays(-1);
+                }
+
+                //GETTING SPRINTS + SPRINT IDS
+                List<Sprint> sprints = db.Sprints.Where(x => x.ProjectID == ProjectID &&
+                                                        (x.StartDate.Date >= firstDayOfMonth.Date && x.EndDate.Date <= lastDayOfMonth.Date))
+                                                        .OrderBy(x => x.StartDate).ToList();
+                int[] sprintIds = new int[] { 0 };
+                if(sprints!= null && sprints.Count>0)
+                {
+                    sprintIds = sprints.Select(x => x.SprintID).ToArray();
+                }
+
+                //Getting Employees and Tasks
+                List<Employee> employees = db.Employees.Where(x => x.DepartmentID == department.DepartmentID).ToList();
+                List<Task> tasks = db.Tasks.Where(x => x.SprintID.HasValue && sprintIds.Contains(x.SprintID.Value)).ToList();
+                #endregion
+            }
+
+
+
             byte[] result = new byte[10000];
 
             return result;
