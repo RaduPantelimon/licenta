befe61fa3b73731565fd789c73db7a55b28e25de
serafim_tanasescu <radu_mihai.bucur@yahoo.com>
2017-04-14 17:48:13 +0300

    Added Excel Generator Styles

--- ResourceApplicationTool/Utils/ExcelReportGenerator.cs
+++ ResourceApplicationTool/Utils/ExcelReportGenerator.cs
@@ -64,8 +64,15 @@ namespace ResourceApplicationTool.Utils
                 {
                     var workbookpart = document.AddWorkbookPart();
                     workbookpart.Workbook = new Workbook();
+                    //add Style
+                    WorkbookStylesPart stylePart = workbookpart.AddNewPart<WorkbookStylesPart>();
+                    stylePart.Stylesheet = GenerateStylesheet();
+                    stylePart.Stylesheet.Save();
+
                     var worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
                     var sheetData = new SheetData();
+
+
 
                     worksheetPart.Worksheet = new Worksheet(sheetData);
 
@@ -83,8 +90,10 @@ namespace ResourceApplicationTool.Utils
 
 
                     //create header rows
+                    DateTime monthDate = new DateTime(year, month, 1);
+
                     List<string> MergeableRows = new List<string>();
-                    Row projectRow = CreateProjectRow(firstDayOfMonth, lastDayOfMonth, project, MergeableRows, 1);
+                    Row projectRow = CreateProjectRow(firstDayOfMonth, lastDayOfMonth, project, MergeableRows, monthDate, 1);
                     Row sprintRow = CreateSprintRow(firstDayOfMonth,lastDayOfMonth,sprints, MergeableRows, 2);
                     Row headerRow = CreateDatesRow(firstDayOfMonth, lastDayOfMonth, 3);
 
@@ -122,6 +131,9 @@ namespace ResourceApplicationTool.Utils
                     //merging cells
                     worksheetPart.Worksheet.InsertAfter(mergeCells, worksheetPart.Worksheet.Elements<SheetData>().First());
 
+
+
+
                     //save data
                     worksheetPart.Worksheet.Save();
                     document.Close();
@@ -135,10 +147,13 @@ namespace ResourceApplicationTool.Utils
 
         private static Row CreateSprintRow(DateTime firstDayOfMonth,DateTime lastDayOfMonth, List<Sprint> sprints,List<string> mergeableRows, int rowIndex)
         {
-            int index = 0;
+            int index = -1;
             Row sprintsRow = new Row();
             sprintsRow.RowIndex = (UInt32)rowIndex;
             DateTime dt = firstDayOfMonth;
+
+            Cell firstcell = CreateCell("", ref index, rowIndex, 3);
+            sprintsRow.AppendChild(firstcell);
 
             int weekCount = 0;
             while (dt.Date <= lastDayOfMonth.Date)
@@ -160,15 +175,18 @@ namespace ResourceApplicationTool.Utils
                     mergeableRows.Add(ColumnLetter(index+1) + rowIndex);
                 }
 
-                Cell newcell = CreateCell(text, ref index, rowIndex);
+                Cell newcell = CreateCell(text, ref index, rowIndex,3);
 
                 dt = dt.AddDays(1);
                 sprintsRow.AppendChild(newcell);
             }
 
+            Cell lastcell = CreateCell("", ref index, rowIndex, 3);
+            sprintsRow.AppendChild(lastcell);
+
             return sprintsRow;
         }
-        private static Row CreateProjectRow(DateTime firstDayOfMonth, DateTime lastDayOfMonth, Project project, List<string> mergeableRows, int rowIndex)
+        private static Row CreateProjectRow(DateTime firstDayOfMonth, DateTime lastDayOfMonth, Project project, List<string> mergeableRows, DateTime monthDate, int rowIndex)
         {
             int index = -1;
             Row projectRow = new Row();
@@ -177,13 +195,14 @@ namespace ResourceApplicationTool.Utils
 
             //adding first cell
             mergeableRows.Add(ColumnLetter(index + 1) + rowIndex);
-            Cell firstcell = CreateCell(project.Title, ref index, rowIndex);
+            Cell firstcell = CreateCell("  Activity report for " + 
+                project.Title + " during " + monthDate.ToString("MMMM yyyy"), ref index, rowIndex,4);
             projectRow.AppendChild(firstcell);
 
             while (dt.Date <= lastDayOfMonth.Date)
             {
                 string text = "";
-                Cell newcell = CreateCell(text, ref index, rowIndex);
+                Cell newcell = CreateCell(text, ref index, rowIndex,4);
 
                 dt = dt.AddDays(1);
                 projectRow.AppendChild(newcell);
@@ -194,19 +213,23 @@ namespace ResourceApplicationTool.Utils
 
         private static Row CreateDatesRow(DateTime firstDayOfMonth, DateTime lastDayOfMonth, int rowIndex)
         {
-            int index = 0;
+            int index = -1;
             Row headerRow = new Row();
             headerRow.RowIndex = (UInt32)rowIndex;
             DateTime dt = firstDayOfMonth;
+
+            Cell firstcell = CreateCell("", ref index, rowIndex, 2);
+            headerRow.AppendChild(firstcell);
+
             while (dt.Date <= lastDayOfMonth.Date)
             {
 
-                Cell newcell = CreateCell(dt.ToString("dd/MM/yyyy"), ref index, rowIndex);
+                Cell newcell = CreateCell(dt.ToString("dd/MM/yyyy"), ref index, rowIndex,2);
 
                 dt = dt.AddDays(1);
                 headerRow.AppendChild(newcell);
             }
-            Cell totalcell = CreateCell("Total", ref index, rowIndex);
+            Cell totalcell = CreateCell("Total", ref index, rowIndex,2);
             headerRow.AppendChild(totalcell);
 
             return headerRow;
@@ -220,7 +243,7 @@ namespace ResourceApplicationTool.Utils
             int total = 0, rowIndex = -1;
             DateTime day = firstDayOfMonth;
 
-            Cell titleCell = CreateCell(employee.FirstName + " " + employee.LastName, ref rowIndex, colIndex);
+            Cell titleCell = CreateCell(employee.FirstName + " " + employee.LastName, ref rowIndex, colIndex,2);
             row.AppendChild(titleCell);
 
             //preparing the formula
@@ -234,9 +257,19 @@ namespace ResourceApplicationTool.Utils
                 {
                     hours = currentTask.Estimation.Value;
                 }
-                Cell newcell = CreateNumberContentCell(hours, ref rowIndex, colIndex);
+                if(day.DayOfWeek == DayOfWeek.Saturday || day.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    Cell newcell = CreateNumberContentCell(hours, ref rowIndex, colIndex,5);
+                    row.AppendChild(newcell);
+                }
+                else
+                {
+                    Cell newcell = CreateNumberContentCell(hours, ref rowIndex, colIndex);
+                    row.AppendChild(newcell);
+                }
+                
                 day = day.AddDays(1);
-                row.AppendChild(newcell);
+                
                 total += hours;
             }
             string finish = ColumnLetter(rowIndex) + colIndex;
@@ -250,31 +283,26 @@ namespace ResourceApplicationTool.Utils
             return row;
         }
 
-        private static Cell CreateNumberContentCell(int value, ref int index, int colIndex)
+        private static Cell CreateNumberContentCell(int value, ref int index, int colIndex, uint styleIndex = 0)
         {
             Cell cell = new Cell();
             cell.DataType = CellValues.Number;
+            cell.StyleIndex = styleIndex;
             // Column A1, 2, 3 ... and so on
             cell.CellReference = ColumnLetter(++index) + colIndex;
-            // Create Text object
-            /*Text t = new Text();
-            t.Text = value;
-
-            // Append Text to InlineString object
-            InlineString inlineString = new InlineString();
-            inlineString.AppendChild(t);*/
 
             // Append InlineString to Cell
             cell.CellValue = new CellValue(value.ToString());
             return cell;
         }
 
-        private static Cell CreateCell(string value, ref int index, int colIndex)
+        private static Cell CreateCell(string value, ref int index, int colIndex, uint styleIndex = 0)
         {
             Cell cell = new Cell();
             cell.DataType = CellValues.InlineString;
             // Column A1, 2, 3 ... and so on
             cell.CellReference = ColumnLetter(++index) + colIndex;
+            cell.StyleIndex = styleIndex;
             // Create Text object
             Text t = new Text();
             t.Text = value;
@@ -287,16 +315,16 @@ namespace ResourceApplicationTool.Utils
             cell.AppendChild(inlineString);
             return cell;
         }
-        private static Cell CreateFormulaCell(string Formula, string value, ref int index, int colIndex)
+        private static Cell CreateFormulaCell(string Formula, string value, ref int index, int colIndex, uint styleIndex = 0)
         {
             Cell cell = new Cell() { CellReference = ColumnLetter(++index) + colIndex };
             CellFormula cellformula = new CellFormula();
             cellformula.Text = Formula;
             CellValue cellValue = new CellValue();
             cellValue.Text = value;
+            cell.StyleIndex = styleIndex;
             cell.Append(cellformula);
             cell.Append(cellValue);
-
             return cell;
         }
 
@@ -315,5 +343,79 @@ namespace ResourceApplicationTool.Utils
             return string.Concat(firstLetter, secondLetter,
                 thirdLetter).Trim();
         }
+
+
+        private static Stylesheet GenerateStylesheet()
+         {
+             Stylesheet styleSheet = null;
+
+             Fonts fonts = new Fonts(
+                new Font( // Index 0 - default
+                    new FontSize() { Val = 10 }
+
+                ),
+                new Font( // Index 1 - header
+                    new FontSize() { Val = 10 },
+                    new Bold(),
+                    new Color() { Rgb = "FFFFFF" }
+
+                ),
+                 new Font(
+                    new FontSize() { Val = 10 },
+                    new Bold(),
+                    new Color() { Rgb = "FFFFFF" }
+
+                ),
+                 new Font(
+                    new FontSize() { Val = 16 },
+                    new Bold(),
+                    new Color() { Rgb = "FFFFFF" }
+
+                ));
+
+            Fills fills = new Fills(
+                     new Fill(new PatternFill() { PatternType = PatternValues.None }), // Index 0 - default
+                     new Fill(new PatternFill() { PatternType = PatternValues.Gray125 }), // Index 1 - default
+                     new Fill(new PatternFill(new ForegroundColor { Rgb = new HexBinaryValue() { Value = "66666666" } })
+                     { PatternType = PatternValues.Solid }), // Index 2 - header
+                    new Fill(new PatternFill(new ForegroundColor { Rgb = new HexBinaryValue() { Value = "428BCA" } })
+                       { PatternType = PatternValues.Solid }), //projects
+                    new Fill(new PatternFill(new ForegroundColor { Rgb = new HexBinaryValue() { Value = "DDDDDD" } })
+                    { PatternType = PatternValues.Solid }),//weekend Days
+                     new Fill(new PatternFill(new ForegroundColor { Rgb = new HexBinaryValue() { Value = "47C9AF" } })
+                     { PatternType = PatternValues.Solid })//sprints
+                 );
+
+             Borders borders = new Borders(
+                     new Border(), // index 0 default
+                     new Border( // index 1 black border
+                         new LeftBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+                         new RightBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+                         new TopBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+                         new BottomBorder(new Color() { Auto = true }) { Style = BorderStyleValues.Thin },
+                         new DiagonalBorder())
+                 );
+
+             CellFormats cellFormats = new CellFormats(
+                     new CellFormat(), // default
+                     new CellFormat { FontId = 0, FillId = 0, BorderId = 1, ApplyBorder = true, ApplyFont = true }, // body
+                     new CellFormat {
+                         Alignment = new Alignment() { Horizontal = HorizontalAlignmentValues.Center, Vertical = VerticalAlignmentValues.Center },
+                         FontId = 1, FillId = 2, BorderId = 1, ApplyFill = true, ApplyFont = true, ApplyAlignment=true}, // employees / dates
+                     new CellFormat{
+                         Alignment = new Alignment() { Horizontal = HorizontalAlignmentValues.Center, Vertical = VerticalAlignmentValues.Center },
+                        FontId = 1, FillId = 5, BorderId = 1, ApplyFill = true, ApplyBorder = true, ApplyFont = true, ApplyAlignment=true }, // sprints
+                     new CellFormat { FontId = 3, FillId = 3, BorderId = 1, ApplyFill = true, ApplyFont = true }, // project
+                     new CellFormat { FontId = 0, FillId = 4, BorderId = 1, ApplyFill = true, ApplyBorder = true, ApplyFont = true }// weekend
+                 );
+
+             styleSheet = new Stylesheet(fonts, fills, borders, cellFormats);
+
+             return styleSheet;
+         }
+
+
+     
     }
 }
+ 
