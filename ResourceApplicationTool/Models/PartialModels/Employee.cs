410d266c8ae76ad0ead563d3e1324adcaa997566
serafim_tanasescu <radu_mihai.bucur@yahoo.com>
2017-04-22 22:10:53 +0300

    Added Employee Buddy Class

--- ResourceApplicationTool/Models/PartialModels/Employee.cs
+++ ResourceApplicationTool/Models/PartialModels/Employee.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace ResourceApplicationTool.Models
 {
@@ -9,9 +11,49 @@ namespace ResourceApplicationTool.Models
     using System.Collections.Generic;
     using Newtonsoft.Json;
 
+    [MetadataType(typeof(EmployeeMD))]
     public partial class Employee
     {
         //used to send values to and from the model during binding and page rendering
         public IList<SkillLevel> SkillLevelsList { get; set; }
+    }
+
+    public class EmployeeMD
+    {
+        [JsonIgnore]
+        public string Password { get; set; }
+        [JsonIgnore]
+        public Nullable<int> ManagerID { get; set; }
+        [JsonIgnore]
+        public Nullable<int> DepartmentID { get; set; }
+        [JsonIgnore]
+        public string CNP { get; set; }
+
+        [JsonIgnore]
+        public decimal Salary { get; set; }
+        [JsonIgnore]
+        public decimal PriorSalary { get; set; }
+        [JsonIgnore]
+        public Nullable<decimal> LastRaise { get; set; }
+        [JsonIgnore]
+        public Nullable<System.DateTime> TerminationDate { get; set; }
+        public string Administrator { get; set; }
+        public Nullable<System.Guid> ProfileImageID { get; set; }
+        [JsonIgnore]
+        public virtual Department Department { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<Education> Educations { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<Task> Tasks { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<Employee> Employees1 { get; set; }
+        [JsonIgnore]
+        public virtual Employee Employee1 { get; set; }
+        [JsonIgnore]
+        public virtual Role Role { get; set; }
+        [JsonIgnore]
+        public virtual File File { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<SkillLevel> SkillLevels { get; set; }
     }
 }
