ca28d19bbd5000179bde8b966f8ded5042252f8e
raduBucur <raduBucur@DESKTOP-EH4CT21>
2017-06-27 14:59:41 -0700

    added roles index page

---
+++ ResourceApplicationTool/Controllers/RolesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ResourceApplicationTool.Models;
+using ResourceApplicationTool.Utils;
+
+namespace ResourceApplicationTool.Controllers
+{
+    public class RolesController : Controller
+    {
+        private RATV3Entities db = new RATV3Entities();
+
+        // GET: Roles
+        public ActionResult Index()
+        {
+            ViewBag.userAccess = Session[Const.CLAIM.USER_ACCESS_LEVEL];
+            return View(db.Roles.ToList());
+        }
+
+        // GET: Roles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Role role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.userAccess = Session[Const.CLAIM.USER_ACCESS_LEVEL];
+            return View(role);
+        }
+
+        // GET: Roles/Create
+        public ActionResult Create()
+        {
+            ViewBag.userAccess = Session[Const.CLAIM.USER_ACCESS_LEVEL];
+            return View();
+        }
+
+        // POST: Roles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RoleID,Name,JobDescription,AverageSalary")] Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Roles.Add(role);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.userAccess = Session[Const.CLAIM.USER_ACCESS_LEVEL];
+            return View(role);
+        }
+
+        // GET: Roles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Role role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.userAccess = Session[Const.CLAIM.USER_ACCESS_LEVEL];
+            return View(role);
+        }
+
+        // POST: Roles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RoleID,Name,JobDescription,AverageSalary")] Role role)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(role).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.userAccess = Session[Const.CLAIM.USER_ACCESS_LEVEL];
+            return View(role);
+        }
+
+        // GET: Roles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Role role = db.Roles.Find(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.userAccess = Session[Const.CLAIM.USER_ACCESS_LEVEL];
+            return View(role);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Role role = db.Roles.Find(id);
+            db.Roles.Remove(role);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ResourceApplicationTool/Models/PartialModels/Role.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ResourceApplicationTool.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    [MetadataType(typeof(RoleMD))]
+    public partial class Role
+    {
+    }
+
+
+    public class RoleMD
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Job Description")]
+        public string JobDescription { get; set; }
+
+        [Required]
+        [Display(Name = "Average Salary")]
+        [DataType(DataType.Currency)]
+        public decimal AverageSalary { get; set; }
+    }
+}
